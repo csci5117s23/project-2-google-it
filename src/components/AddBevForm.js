2246cb5ae50875996d5391262887e7e49f94bc45
Jonathan Leibovich <60627566+leamur88@users.noreply.github.com>
2023-04-16 13:59:13 -0500

    Uploading to s3

--- src/components/AddBevForm.js
+++ src/components/AddBevForm.js
@@ -1,9 +1,17 @@
 import React, {useEffect, useState} from 'react'
 import { GoogleMap, LoadScript, MarkerF, InfoWindow, Marker } from '@react-google-maps/api';
 import Resizer from "react-image-file-resizer";
+var AWS = require('aws-sdk');
 
 const API_ENDPOINT = process.env.NEXT_PUBLIC_BACKEND_BASE_URL;
 const API_KEY = process.env.NEXT_PUBLIC_BACKEND_API_KEY
+const BUCKET = process.env.NEXT_PUBLIC_BUCKET_NAME;
+const IDENTITY_POOL_ID = process.env.NEXT_PUBLIC_IDENTITY_POOL_ID
+AWS.config.region = 'us-east-2'; // Region
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: IDENTITY_POOL_ID,
+});
+
 
 export default function AddBevForm(){
 	function handlePhotoUpload(){
@@ -31,32 +39,18 @@ export default function AddBevForm(){
 	const handleSubmit = async (event) => {
 		event.preventDefault();
 		const img = document.getElementsByClassName('file-input')[0].files[0]
-		const bevName = document.getElementById('bevName')
-		const bevLocation = document.getElementById('bevLocation')
-		const bevRating = document.getElementById('bevRating')
-		const bevDescription = document.getElementById('bevDescription')
+		const bevName = document.getElementById('bevName').value
+		const bevLocation = document.getElementById('bevLocation').value
+		const bevRating = document.getElementById('bevRating').value
+		const bevDescription = document.getElementById('bevDescription').value
 		const resizedImage = null;
-		// S/o Upper 5
-		Resizer.imageFileResizer(
-            img, //file name
-            300, //max pixel width
-            300, //max pixel height
-            "JPEG", //compression format
-            100, //quality
-            0, //rotation
-            (resizedFile) => {
-                //Callback function
-				resizedFile = URL.createObjectURL(resizedFile);
-            },
-            "file" //output type
-        );
 
+		var imgLocation = "bevary/user/"
 		const uploadData = {
-			bevName: bevName,
-			rating: bevRating,
-			locName: bevLocation,
-			desc: bevDescription,
-			img: resizedImage
+			"bevName": bevName,
+			"locName": bevLocation,
+			"rating": bevRating,
+			"desc": bevDescription
 		}
 		const backendResponse = await fetch(API_ENDPOINT + "/bevEntry", {
 			'method':'POST',
@@ -66,16 +60,50 @@ export default function AddBevForm(){
 			},
 			body: JSON.stringify(uploadData)
 		  })
+
 		  
 		const backendData = await backendResponse.json()
-		const bevID = data['_id']
-		console.log(event)
+		const bevID = backendData['_id']
+		imgLocation += bevID + '/' + img.name
+		console.log(imgLocation, img)
+
+		var upload = new AWS.S3.ManagedUpload({
+			params: {
+			  Bucket: BUCKET,
+			  Key: imgLocation,
+			  Body: img
+			}
+		});		
+		var promise = upload.promise();
+		promise.then(
+			async function(data) {
+				const imgURL = "https://csci5117-project1-rankit.s3.us-east-2.amazonaws.com/" + imgLocation
+				console.log("IMG URL", imgURL)
+				const imdLocData = {
+					"imgURL": imgURL
+				}
+				const updateImageResponse = await fetch(API_ENDPOINT + "/bevEntry/" + bevID, {
+					'method':'PATCH',
+					'headers': {
+						'x-apikey': API_KEY,
+						'Content-Type': 'application/json'
+					},
+					body: JSON.stringify(imdLocData)
+				  })
+				  const updateImageData = await updateImageResponse.json()
+				  console.log(updateImageData)
+			},
+			function(err) {
+			  return alert("There was an error uploading your photo: ", err.message);
+			}
+		);
 	}
 
 
 
 	return (
 	<>
+	    <script src="https://sdk.amazonaws.com/js/aws-sdk-2.1359.0.js"></script>
 		<form>
 		<div class="columns">
 			<div class="column">
