48dfbc3236ddaff543bd3bbaf870d8c60d9fc898
Jonathan Leibovich <60627566+leamur88@users.noreply.github.com>
2023-04-15 23:40:33 -0500

    Form image preview working

--- src/components/AddBevForm.js
+++ src/components/AddBevForm.js
@@ -2,16 +2,44 @@ import React, {useEffect, useState} from 'react'
 import { GoogleMap, LoadScript, MarkerF, InfoWindow, Marker } from '@react-google-maps/api';
 
 export default function AddBevForm(){
+	function handlePhotoUpload(){
+		const fileInput = document.getElementsByClassName('file-input')[0]
+		const photoUploadDiv = document.getElementById('photoUpload')
+		const img = document.getElementById('imgPreview')
+		const imgDiv = document.getElementById('photoPreviewDiv')
+		imgDiv.setAttribute("class", "")
+		img.setAttribute("src", URL.createObjectURL(fileInput.files[0]))
+		img.onload = function() {
+			URL.revokeObjectURL(img.src) // free memory
+		}
+		photoUploadDiv.setAttribute("class", "is-invisible")
+	}
+
+	function handleNewPhoto(){
+		const photoUploadDiv = document.getElementById('photoUpload')
+		const imgDiv = document.getElementById('photoPreviewDiv')
+		imgDiv.setAttribute("class", "is-hidden")
+		photoUploadDiv.setAttribute("class", "file is-large is-boxed")
+		const fileInput = document.getElementsByClassName('file-input')[0]
+		fileInput.value = ''
+	}
+
+
+
 	return (
 	<>
 		<div class="columns">
 			<div class="column">
-				<div class="file is-large is-boxed">
+				<div class="is-hidden" id='photoPreviewDiv'>
+					<img class="image is-128x128" id='imgPreview' />
+					<button class="button is-large is-danger is-fullwidth" onClick={handleNewPhoto}>Change Photo</button>
+				</div>
+				<div id='photoUpload' class="file is-large is-boxed">
 					<label class="file-label">
-						<input class="file-input" type="file" name="resume" />
+						<input class="file-input" type="file" accept='image/*' name="bevPhoto" onChange={handlePhotoUpload} />
 						<span class="file-cta">
-						<span class="file-icon">
-							<i class="fas fa-upload"></i>
+						<span class="icon is-large">
+							<img src='https://cdn-icons-png.flaticon.com/512/3566/3566345.png'/>
 						</span>
 						<span class="file-label">
 							Upload a picture!
