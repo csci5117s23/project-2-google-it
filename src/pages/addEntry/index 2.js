cd9f9bb0ceb439c80aebb9ab6207d5d86b6ec2a2
Jonathan Leibovich <60627566+leamur88@users.noreply.github.com>
2023-04-18 14:33:49 -0500

    adding it back

---
+++ src/pages/addEntry/index 2.js
@@ -0,0 +1,29 @@
+import React, {useEffect, useState} from 'react'
+import { GoogleMap, LoadScript, MarkerF, InfoWindow, Marker } from '@react-google-maps/api';
+import { useAuth } from "@clerk/nextjs";
+import Header from '@/components/header';
+import AddBevForm from '@/components/AddBevForm';
+
+const API_ENDPOINT = process.env.NEXT_PUBLIC_BACKEND_BASE_URL;
+const API_KEY = process.env.NEXT_PUBLIC_BACKEND_API_KEY
+
+export default function AddEntryPage (){
+	const [loading, setLoading] = useState(false)
+	if(loading){
+
+		return (<>
+			<Header title={"Bevary"} />
+			<div>Loading...</div>
+		</>	
+		)
+
+	}
+	else{
+		return(
+			<>
+			<Header title={"Bevary"} />
+			<AddBevForm />
+			</>
+		)
+	}
+}
