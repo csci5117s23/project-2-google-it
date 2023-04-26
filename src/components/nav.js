fe8532ed26fddcb1479c740c184563b7d7d833a3
MikeC1419 <51382283+MikeC1419@users.noreply.github.com>
2023-04-26 11:41:30 -0500

    Update nav.js

--- src/components/nav.js
+++ src/components/nav.js
@@ -10,33 +10,7 @@ export default function NavBar(){
 				<Link href="/addEntry"><div class="elevate"><img class="plus" src="https://library.uwosh.edu/images/plus-icon.png/@@images/image.png"></img></div></Link>
 				<Link class="sidebutton" href="/map"><div class="stack"><img class="sideImg" src="https://cdn-icons-png.flaticon.com/512/854/854878.png"></img><p>Map</p></div></Link>
 			</div>
-			{/* <div class="navbar-menu">
-				<div class="navbar-start">
-					<div class="navbar-item">
-						<a class="navbar-link">
-						Left
-						</a>
-					</div>
-				</div>
-				{pages.map(pg =>{
-            		return <div class="navbar-item">
-						{pg}
-					</div>
-            		// if(todo.complete){
-                	// 	return <TodoItem complete={true} task={todo.task} desc={todo.description} date={todo.created} onTodoClick = {completeTask}></TodoItem>
-            		// }
-            		// //return <TodoItem num={todo.num} complete={todo.complete} task={todo.task} date={todo.created} ></TodoItem>
-            
-            		// return <TodoItem complete={false} task={todo.task} desc={todo.description} date={todo.created} onTodoClick = {completeTask}></TodoItem>
-        		})}
-				<div class="navbar-end">
-					<div class="navbar-item">
-						<a class="navbar-link">
-						Right
-						</a>
-					</div>
-				</div>
-			</div> */}
+			
 		</nav>
 	</>)
 }
