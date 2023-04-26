ec794beafff926a60c9863383e73ec6c4a669f3e
Jonathan Leibovich <60627566+leamur88@users.noreply.github.com>
2023-04-26 12:33:47 -0500

    loading view page

--- src/components/viewEntry.js
+++ src/components/viewEntry.js
@@ -1,40 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router'
 import { useAuth } from "@clerk/nextjs";
-const API_ENDPOINT = process.env.NEXT_PUBLIC_BACKEND_BASE_URL;
 
-export default function ViewEntry(){
-	const [data, setData] = useState(null);
-	const [loading, setLoading] = useState(null);
-	const { isLoaded, userId, sessionId, getToken } = useAuth();
-	const router = useRouter()
-	const { id } = router.query
-	
-
-	useEffect(() => {
-		
-		const fetchData = async () => {
-			const token = await getToken({template: "BevaryTemplate"})
-			console.log(id)
-			const response = await fetch(API_ENDPOINT + "/bevEntry/" + id, {
-				'method':'GET',
-				'headers': {'Authorization': 'Bearer ' + token}
-		  	})
-		  const data = await response.json()
-		  // update state -- configured earlier.
-		  console.log(data)
-		  setData(data)
-		  setLoading(false)
-		}
-		fetchData();
-	}, [])
-
-	if (loading) {
-		return (<>
-			<div>Loading!</div>
-		</>)
-	}
-	else{
-		// console.log("Made it!")
-	}
+export default function ViewEntry({data}){
+	// const { isLoaded, userId, sessionId, getToken } = useAuth();
+	console.log(data)
 }
--- src/pages/view/[id].js
+++ src/pages/view/[id].js
@@ -5,17 +5,49 @@ import Header from '@/components/header';
 import NavBar from '@/components/nav';
 import ViewEntry from '@/components/viewEntry';
 
+const API_ENDPOINT = process.env.NEXT_PUBLIC_BACKEND_BASE_URL;
 
 export default function ViewPage(){
 	const { isLoaded, userId, sessionId, getToken } = useAuth();
+	const [data, setData] = useState(null);
+	const [loading, setLoading] = useState(true);
 	const router = useRouter()
 	const { id } = router.query
+	useEffect(() => {
+		if (router.isReady){
+			const fetchData = async () => {
+				const token = await getToken({template: "BevaryTemplate"})
+				console.log("Inside of useEffect!", router.isReady)
+				const response = await fetch(API_ENDPOINT + "/bevEntry/" + id, {
+					'method':'GET',
+					'headers': {'Authorization': 'Bearer ' + token}
+				})
+			const data = await response.json()
+			// update state -- configured earlier.
+			console.log(data)
+			setData(data)
+			setLoading(false)
+			}
+			fetchData();
+		
+			
+		}
+		
+	}, [router.isReady])
+	if (loading) {
+		return(<>
+			<Header title={"Bevary"}/>
+			<div>Loading!</div>
+			<NavBar/>
+		</>)
 
-	return(<>
-		<Header title={"Bevary"}/>
-		<div>{id}</div>
-		<ViewEntry id={id} />
-		<NavBar/>
-	</>)
+	} else {
+		return(<>
+			<Header title={"Bevary"}/>
+			<ViewEntry data={data}/>
+			<NavBar/>
+		</>)
+
+	}
 }
 
