3c41121174b47289a883d677c522d7538056ce22
MikeC1419 <51382283+MikeC1419@users.noreply.github.com>
2023-04-26 11:38:39 -0500

    Update nav.js

--- src/components/nav.js
+++ src/components/nav.js
@@ -9,9 +9,6 @@ export default function NavBar(){
 				<Link class="sidebutton" href="/list"><div class="stack"><img class="sideImg" src="https://cdn-icons-png.flaticon.com/512/3221/3221593.png"></img><p>Journal</p></div></Link> 
 				<Link href="/addEntry"><div class="elevate"><img class="plus" src="https://library.uwosh.edu/images/plus-icon.png/@@images/image.png"></img></div></Link>
 				<Link class="sidebutton" href="/map"><div class="stack"><img class="sideImg" src="https://cdn-icons-png.flaticon.com/512/854/854878.png"></img><p>Map</p></div></Link>
-				{/* <div class="sidebutton"><Link href="/list">Journal</Link></div>
-				<div class="plus"><Link href="/addEntry"><div class="elevate">addEntry</div></Link></div>
-				<div class="sidebutton"><Link href="/map">Map</Link></div> */}
 			</div>
 			{/* <div class="navbar-menu">
 				<div class="navbar-start">
