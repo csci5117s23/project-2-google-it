6be80ff367809ad403ba79d3516e5bd55bd0b3df
MikeC1419 <51382283+MikeC1419@users.noreply.github.com>
2023-04-26 11:38:07 -0500

    Update nav.js

--- src/components/nav.js
+++ src/components/nav.js
@@ -7,7 +7,7 @@ export default function NavBar(){
 		<nav class="bottom" role="navigation" aria-label="navigation">
 			<div class="nav">
 				<Link class="sidebutton" href="/list"><div class="stack"><img class="sideImg" src="https://cdn-icons-png.flaticon.com/512/3221/3221593.png"></img><p>Journal</p></div></Link> 
-				<Link href="/addEntry"><div class="elevate"><img class="plus" src="https://www.pngall.com/wp-content/uploads/10/Plus-Symbol-PNG-Image-HD.png"></img></div></Link>
+				<Link href="/addEntry"><div class="elevate"><img class="plus" src="https://library.uwosh.edu/images/plus-icon.png/@@images/image.png"></img></div></Link>
 				<Link class="sidebutton" href="/map"><div class="stack"><img class="sideImg" src="https://cdn-icons-png.flaticon.com/512/854/854878.png"></img><p>Map</p></div></Link>
 				{/* <div class="sidebutton"><Link href="/list">Journal</Link></div>
 				<div class="plus"><Link href="/addEntry"><div class="elevate">addEntry</div></Link></div>
