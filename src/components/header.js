5702c92e80503b97b2f7d579127486cdfa3b8889
MikeC1419 <51382283+MikeC1419@users.noreply.github.com>
2023-04-27 12:29:39 -0500

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,15 @@
 import { React } from "react";
 import Icon from "./icon";
+import LogoutButton from "./logoutButton";
+
 
 export default function Header({ title }) {
+  
   return (
     <header>
-      <div
+      <div className="header">
+        <div className="sideHeader"></div>
+        <div
         style={{
           textAlign: "center",
           fontSize: "6vh",
@@ -18,7 +23,19 @@ export default function Header({ title }) {
       >
         <Icon />
         {title}
+        
+        </div>
+        <div style={{
+          textAlign: "right", 
+          width: "25%",
+          paddingTop: "15px",
+          paddingRight: "10px",
+        }}>
+          <LogoutButton></LogoutButton>
+        </div>
       </div>
+      
+      
     </header>
   );
 }
