03cbe831e3dd25641e00c6c9d83ad01ce4e91fb4
Jonathan Leibovich <60627566+leamur88@users.noreply.github.com>
2023-04-29 18:56:05 -0500

    have to be signed in

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,30 @@
 import '@/styles/globals.css'
 import '@/styles/nav.css';
 import { ClerkProvider, SignedIn, SignedOut, RedirectToSignIn, SignIn } from '@clerk/nextjs';
+import { useRouter } from 'next/router';
+import Home from '.';
+const publicPages = ["/", "/login", "/logout"];
+
 
 export default function App({ Component, pageProps }) {
+	const router = useRouter();
+	const pathname  = router.pathname;
+	const isPublicPage = publicPages.includes(pathname);
   return( 
   <ClerkProvider {...pageProps}>
-	<Component {...pageProps} />
+	{isPublicPage ? (
+			<Component {...pageProps} />
+			) : (
+			<>
+				<SignedIn>
+				<Component {...pageProps} />
+				</SignedIn>
+				<SignedOut>
+				<Home />
+				</SignedOut>
+
+			</>
+			)}
   </ClerkProvider>
   )
 }
